599b4504a3c2ed1cbe530fff4ce42eef9bc35892
sjessicameyer <sjmeyer@wpi.edu>
2025-02-07 13:40:57 -0500

    Populated cards from database using routes.py; Debugged models

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,13 @@
 from flask import render_template
 import sqlalchemy as sqla
-
+import sqlalchemy.orm as sqlo
 
 from app.main import main_blueprint as main
+from app.main.models import Position
+from app import db
 
 @main.route('/', methods=['GET'])
 @main.route('/index', methods=['GET'])
 def index():
-    return render_template('index.html')
+    positions = db.session.query(Position)
+    return render_template('index.html', positions = positions)
