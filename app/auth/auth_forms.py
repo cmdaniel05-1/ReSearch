54ff59fbd585fdbb057ba80f166a8578580af403
April Zingher <105899713+AprilZingher@users.noreply.github.com>
2025-02-07 16:20:42 -0500

    added faculty validation

--- app/auth/auth_forms.py
+++ app/auth/auth_forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 import sqlalchemy as sqla
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, TextAreaField
+from wtforms import IntegerField, StringField, SubmitField, PasswordField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, Email, Length, ValidationError
 
 from app import db
-from app.main.models import Student
+from app.main.models import Faculty, Student
 
 class LoginForm(FlaskForm) :
     username = StringField('Username', validators = [DataRequired ()])
@@ -16,7 +16,7 @@ class RegistrationForm(FlaskForm):
     username = StringField( 'Username', validators = [DataRequired()])
     firstname = StringField( 'First Name',validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
-    
+    wpi_id = IntegerField('WPI ID', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     address = TextAreaField('Address', [Length(min=0, max=200)])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -28,9 +28,17 @@ class RegistrationForm(FlaskForm):
         student = db.session.scalars(query).first()
         if student is not None:
             raise ValidationError('This username already exists! Please use a diferent username.')
+        query = sqla.select(Faculty).where(Faculty.username == username.data)
+        faculty = db.session.scalars(query).first()
+        if faculty is not None:
+            raise ValidationError('This username already exists! Please use a diferent username.')
         
     def validate_email(self, email):
         query = sqla.select(Student).where(Student.email == email.data)
-        student = db.session.scalars(query).first
+        student = db.session.scalars(query).first()
         if student is not None:
+            raise ValidationError('The email already exists! Please use a different email.')
+        query = sqla.select(Faculty).where(Faculty.email == email.data)
+        faculty = db.session.scalars(query).first()
+        if faculty is not None:
             raise ValidationError('The email already exists! Please use a different email.')
