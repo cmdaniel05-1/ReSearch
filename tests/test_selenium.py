f78223d6dd771ed363e20a2f585952c909ef8f12
sjessicameyer <sjmeyer@wpi.edu>
2025-03-03 01:47:41 -0500

    Progress on selenium tests

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,3 +1,6 @@
+import os
+import sys
+import threading
 import pytest
 
 from selenium import webdriver
@@ -7,26 +10,56 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.ui import Select
-from time import sleep
+import time
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+from app import create_app
 
 #Command to run: pytest tests/test_selenium.py
 
-# User fixure - 1
+# Student fixure - 1
 @pytest.fixture
 def student1():
     return  {'wpi_id':'123456', 'username' : 'testuser', 'firstname':'John', 'lastname':'Doe', 'email':'test@example.com', 'phone_num':'7816662121', 'password' : 'securepassword'}
 
-# User fixure - 2
+# Faculty fixure - 2
 @pytest.fixture
 def faculty1():
     return  {'wpi_id':'789012', 'username' : 'professor', 'firstname':'Alice', 'lastname':'Smith', 'email':'alice@example.com', 'phone_num':'555-1234', 'password' : 'professorpass'}
 
- # Post fixure - 1
+# Post fixure - 1
 @pytest.fixture
 def position1():
-    return {}
+    return {'title' : 'Robotics Engineering Intern', 'description' : 'Build robots & code.', 'req_time' : '5', 'student_count' : '2'}
 
- # Post fixure - 2
 @pytest.fixture
-def position2():
-    return {}
+def browser():
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    driver = webdriver.Chrome(options=options) #use selenium manager
+    driver.implicitly_wait(8)
+    
+    yield driver
+
+    driver.quit()
+
+@pytest.fixture(scope='module')
+def flask_app():
+    def run_app():
+        app = create_app()
+        app.run(port=3000, use_reloader=False)  
+
+    # use threading
+    app_thread = threading.Thread(target=run_app)
+    app_thread.daemon = True 
+    app_thread.start()
+
+    time.sleep(2)
+    
+    yield 
+
+#test that your chrome driver even works
+def test_browser_title(browser, flask_app):
+    browser.get('http://localhost:3000/index')
+
+    assert "ReSearch" in browser.title
